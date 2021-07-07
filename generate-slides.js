4a3c4f616119bad76721f9ca3f92e004aef3a861
twyair <yairt16@gmail.com>
2021-07-07 14:36:00 +0300

    autogenerate the prolog slides

--- generate-slides.js
+++ generate-slides.js
@@ -2,26 +2,33 @@ var fs = require('fs');
 var path = require('path');
 var Handlebars = require("handlebars");
 
-function buildHtml(name) {
+function buildHtml(output_dir, renderTemplate, name) {
     console.log("generating slide for ", name);
-    var template = fs.readFileSync(path.resolve(path.join(__dirname, 'slide-template.hbs')), "utf-8");
-    var renderTemplate = Handlebars.compile(template);
     var html = renderTemplate({tutorial_name: name});
-    fs.writeFile(`./slides/${name}.html`, html, err => {
+    fs.writeFile(`${output_dir}/${name}.html`, html, err => {
         if (err) console.log(err);
         console.log("File written succesfully");
     });
 }
 
-fs.readdir("./md", (err, files) => {
-    if (err) {
-        console.error("Could not list the directory.", err);
-        process.exit(1);
-    }
-  
-    files.forEach((file, _index) => {
-        if (file.endsWith(".md")) {
-            buildHtml(file.substring(0, file.length - 3));
+//
+function generate_slides(output_dir, directory, template_name) {
+    fs.readdir(directory, (err, files) => {
+        if (err) {
+            console.error("Could not list the directory.", err);
+            process.exit(1);
         }
+
+        var template = fs.readFileSync(path.resolve(path.join(__dirname, template_name)), "utf-8");
+        var renderTemplate = Handlebars.compile(template);
+
+        files.forEach((file, _index) => {
+            if (file.endsWith(".md")) {
+                buildHtml(output_dir, renderTemplate, file.substring(0, file.length - 3));
+            }
+        });
     });
-})
+}
+
+generate_slides(path.join(__dirname, "./slides"), path.join(__dirname, "./md"), 'slide-template.hbs');
+generate_slides(path.join(__dirname, "./prolog-slides"), path.join(__dirname, "./prolog-md"), 'prolog-slide-template.hbs');
