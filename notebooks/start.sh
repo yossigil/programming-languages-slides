969c713802dc039f1514646bfb1ffad952a1ab9f
twyair <yairt16@gmail.com>
2021-07-08 15:20:50 +0300

    remove the notebooks dir
    the notebooks are now embedded in the slides

--- notebooks/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-jupyter notebook --NotebookApp.tornado_settings='{"headers":{"Access-Control-Allow-Origin": "*", "Content-Security-Policy": "frame-ancestors self http://localhost:8000/"}}' --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.disable_check_xsrf="True" --no-browser --NotebookApp.port=16789
